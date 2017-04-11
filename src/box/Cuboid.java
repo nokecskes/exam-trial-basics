84c05fb0a4216efc181a3ee1010dd12a051ccc17
Noemi Kecskes <nokecskes@gmail.com>
2017-04-11 14:30:51 +0200

    add cuboid class with methods to calculate surface and volume

---
+++ src/box/Cuboid.java
@@ -0,0 +1,36 @@
+package box;
+
+public class Cuboid {
+  // Create a class that represents a cuboid:
+  // It should take its three dimensions as constructor parameters (numbers)
+  // It should have a method called `getSurface` that returns the cuboid's surface
+  // It should have a method called `getVolume` that returns the cuboid's volume
+
+
+  int a, b, c;
+
+  public Cuboid(int a, int b, int c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  public int getSurface() {
+    int surface = 2 * ((a * b) + (b * c) + (c * a));
+    return surface;
+  }
+
+  public int getVolume() {
+    int volume = a * b * c;
+    return volume;
+  }
+
+  public static void main(String[] args) {
+    Cuboid testCuboid = new Cuboid(2, 4, 5);
+    int testSurface = testCuboid.getSurface();
+    int testVolume = testCuboid.getVolume();
+    System.out.println("Surface is: " + testSurface + ", volume is: " + testVolume);
+  }
+
+
+}
